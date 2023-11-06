86c319a3d1228083cba369fc936cad3b35972f33
CesarAli740 <cesarali740@gmail.com>
2023-11-06 18:22:00 -0400

    cambios

--- Cliente/reservar.php
+++ Cliente/reservar.php
@@ -2,189 +2,224 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Reservas de Mesas</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RESERVAS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <style>
   body {
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 0;
-    background-color: #f2f2f2;
-  }
-  header {
-    background-color: #333;
-    color: white;
-    text-align: center;
-    padding: 1rem;
-  }
-  .container {
-    position: relative;
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 2rem;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
-  .mesa {
-    position: relative;
-    width: 100px;
-    height: 100px;
-    margin: 0.5rem;
-    text-align: center;
-    line-height: 100px;
-    font-weight: bold;
-    cursor: pointer;
-    border-radius: 50%;
-    background-color: #E0E0E0;
-    transition: background-color 0.3s;
-  }
-  .libre {
-    background-color: green;
-  }
-  .ocupada {
-    background-color: red;
-  }
-  .reservada {
-    background-color: yellow;
-  }
-  .reservado-label {
-    position: absolute;
-    bottom: -15px;
-    left: 50%;
-    transform: translateX(-50%);
-    font-size: 10px;
-    color: #333;
-  }
-  form {
-    max-width: 400px;
-    margin: 1rem auto 2rem;
-    padding: 1rem;
-    background-color: white;
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    z-index: 1;
-  }
-  .btn-container {
-    position: fixed;
-    left: 20px;
-    top: 50%;
-    transform: translateY(-50%);
-    display: flex;
-    flex-direction: column;
-  }
-  button {
-    padding: 0.5rem 1rem;
-    font-size: 16px;
-    margin-bottom: 1rem;
-    background-color: #333;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    transition: background-color 0.3s;
-  }
-  button:hover {
-    background-color: #555;
-  }
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+  margin: 0;
+  background: linear-gradient(to right, rgb(128, 74, 0), rgb(255, 162, 0));
+}
+
+.reservation-container {
+  margin: 3rem;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  width: 85%;
+  height: auto;
+  padding: 2rem;
+  background: white;
+  border-radius: 2rem;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0);
+}
+
+.form-container {
+  padding: 2rem;
+  margin: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 1rem;
+}
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 1.2rem;
+}
+select,
+input[type="text"] {
+  width: 100%;
+  padding: 0.8rem;
+  border: 1px solid #ccc;
+  border-radius: 1rem;
+}
+#reserve-button {
+  background: rgb(245, 193, 104);
+  width: 9rem;
+  height: 2.5rem;
+  border-radius: 1rem;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  margin: auto;
+}
+#reserve-button:hover {
+  background: rgb(248, 217, 163);
+  cursor: pointer;
+}
+
+.reservation-details {
+  flex: 1;
+  padding: 2rem;
+  background: #f3f3f3;
+  border-radius: 1rem;
+}
+
+.table-map {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.table {
+  width: 6.5rem;
+  height: 6.5rem;
+  border-radius: 50%;
+  background: #dbd9d9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.table.selected {
+  background: #1cb978;
+  color: white;
+}
+
+@media (max-width: 746px) {
+    .reservation-container {
+        margin: 2rem;
+        display: flex;
+        flex-direction: column;
+    }
+    .table {
+        width: 4rem;
+        height: 4rem;
+    }
+    .form-group{
+        display: flex;
+        gap: 2rem;
+    }
+    .form-container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 1rem;
+    }
+}
+@media (max-width: 425px) {
+    .reservation-container {
+        margin: 1rem;
+        display: flex;
+        flex-direction: column;
+    }
+    .form-group{
+        display: flex;
+        gap: 1rem;
+    }
+}
+@media (max-width: 380px) {
+    .reservation-container {
+        margin: 1rem;
+        display: flex;
+        flex-direction: column;
+        padding: .4rem;
+    }
+    .form-group{
+        display: flex;
+        gap: 1rem;
+    }
+    .reservation-details{
+        padding: .8rem;
+        margin: .6rem;
+    }
+}
 </style>
-</head>
+
 <body>
-  <header>
-    <h1>Reservas de Mesas</h1>
-  </header>
-  <form id="formulario-reserva">
-    <h2>Formulario de Reserva</h2>
-    <label for="nombre">Nombre:</label>
-    <input type="text" id="nombre" name="nombre" required>
-    <label for="mesa">Mesa:</label>
-    <select id="mesa" name="mesa" required>
-      <option value="" disabled selected>Seleccionar mesa</option>
-      <option value="Mesa 1">Mesa 1</option>
-      <option value="Mesa 2">Mesa 2</option>
-      <option value="Mesa 3">Mesa 3</option>
-    </select>
-    <button type="submit">Reservar</button>
-  </form>
-  <div class="btn-container">
-    <button id="agregar-mesa">+</button>
-    <button id="quitar-mesa">-</button>
-  </div>
-  <div class="container">
-    <div class="mesa libre">Mesa 1</div>
-    <div class="mesa libre">Mesa 2</div>
-    <div class="mesa libre">Mesa 3</div>
-  </div>
-  <script>
-    // JavaScript aquí (modificado para mostrar "Reservado")
-    document.addEventListener("DOMContentLoaded", () => {
-      let numeroMesas = 3; // Inicialmente hay 3 mesas
+    <div class="reservation-container">
+        <div class="form-container">
+            <h2>Reserva tu mesa</h2>
+            <div class="form-group">
+                <label for="day">Día:</label>
+                <select id="day" >
+                    <option value="">Selecciona un día</option>
+                    <option value="opcion1">Lunes</option>
+                    <option value="opcion3">Martes</option>
+                    <option value="opcion4">Miercoles</option>
+                    <option value="opcion5">Jueves</option>
+                    <option value="opcion6">Viernes</option>
+                    <option value="opcion7">Sábado</option>
+                    <option value="opcion8">Domingo</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="time">Hora:</label>
+                <input type="time" id="time">
+            </div>
+            <div class="form-group">
+                <label>Costo por mesa:</label>
+                <span id="cost">50 dólares</span>
+            </div>
+            <div class="form-group">
+                <label>Total:</label>
+                <span id="total">0 dólares</span>
+            </div>
+            <button id="reserve-button" onclick="reserveTable()">Reservar</button>
+        </div>
+        <div class="reservation-details">
+            <h2>Mapa de mesas</h2>
+            <div class="table-map">
+                <!-- Generar mesas aquí -->
+            </div>
+        </div>
+    </div>
 
-      const mesas = document.querySelectorAll(".mesa");
-      const formularioReserva = document.querySelector("#formulario-reserva");
-      const botonAgregarMesa = document.querySelector("#agregar-mesa");
-      const botonQuitarMesa = document.querySelector("#quitar-mesa");
-      const selectMesa = document.querySelector("#mesa");
 
-      // Evento al hacer clic en una mesa
-      mesas.forEach(mesa => {
-        mesa.addEventListener("click", () => {
-          const mesaSeleccionada = mesa.textContent;
-          const nombre = document.querySelector("#nombre").value;
+    <script>
+        // Función para calcular el costo total
+        function calculateTotal() {
+            const selectedTables = document.querySelectorAll(".table.selected");
+            const costPerTable = 50;
 
-          if (mesa.classList.contains("libre")) {
-            mesa.textContent = ` ${mesaSeleccionada} - Reservado ${nombre}`;
-            mesa.classList.remove("libre");
-            mesa.classList.add("reservada");
-          }
-        });
-      });
+            const totalTables = selectedTables.length;
+            const totalCost = totalTables * costPerTable;
 
-      // Evento al enviar el formulario de reserva
-      formularioReserva.addEventListener("submit", function(event) {
-        event.preventDefault(); // Evitar que se recargue la página
-
-        const nombre = document.querySelector("#nombre").value;
-        const mesaSeleccionada = selectMesa.value;
-        const mesa = document.querySelector(`.mesa:contains('${mesaSeleccionada}')`);
-
-        if (mesa) {
-          mesa.textContent = `Mesa ${mesaSeleccionada} - Reservado por ${nombre}`;
-          mesa.classList.remove("libre");
-          mesa.classList.add("reservada");
-          alert(`¡Reserva realizada por ${nombre} en ${mesaSeleccionada}!`);
+            document.getElementById("total").textContent = totalCost + " dólares";
         }
-      });
 
-      // Evento para agregar mesa
-      botonAgregarMesa.addEventListener("click", () => {
-        numeroMesas++;
-        const nuevaMesa = document.createElement("div");
-        nuevaMesa.classList.add("mesa", "libre");
-        nuevaMesa.textContent = `Mesa ${numeroMesas}`;
-        
-        nuevaMesa.addEventListener("click", () => {
-          const nombre = document.querySelector("#nombre").value;
-          nuevaMesa.textContent = `Mesa ${numeroMesas} - Reservado por ${nombre}`;
-          nuevaMesa.classList.remove("libre");
-          nuevaMesa.classList.add("reservada");
-        });
+        // Función para reservar mesa
+        function reserveTable() {
+            // Resto del código para recopilar los valores del formulario
 
-        selectMesa.innerHTML += `<option value="Mesa ${numeroMesas}">Mesa ${numeroMesas}</option>`;
-        document.querySelector(".container").appendChild(nuevaMesa);
-      });
+            // Llamamos a la función para calcular el costo total
+            calculateTotal();
 
-      // Evento para quitar mesa
-      botonQuitarMesa.addEventListener("click", () => {
-        const ultimaMesa = document.querySelector(".container").lastChild;
-        if (ultimaMesa && ultimaMesa.classList.contains("mesa")) {
-          selectMesa.removeChild(selectMesa.lastChild);
-          document.querySelector(".container").removeChild(ultimaMesa);
-          numeroMesas--;
+            // Resto del código para procesar la reserva
         }
-      });
-    });
-  </script>
+
+        // Código para generar las mesas
+        const tableMap = document.querySelector(".table-map");
+        const numberOfTables = 8; // Puedes ajustar la cantidad de mesas aquí
+
+        for (let i = 1; i <= numberOfTables; i++) {
+            const table = document.createElement("div");
+            table.classList.add("table");
+            table.textContent = i;
+            table.addEventListener("click", () => {
+                table.classList.toggle("selected");
+                calculateTotal(); // Llamamos a la función para recalcular el costo total al seleccionar/deseleccionar mesas
+            });
+            tableMap.appendChild(table);
+        }
+    </script>
 </body>
 </html>
